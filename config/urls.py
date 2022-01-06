958d326de49d77495ec3e33cc4c50f0a47b4fd58
MahdiyehZamani <90906871+mahdiyehz@users.noreply.github.com>
2022-01-06 21:52:32 +0330

    Update config urls

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts.views import login_success
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('login_success/', login_success, name='login_success'),
+    path('', include('food_ordering.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
